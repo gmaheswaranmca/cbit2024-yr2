19a0f21c0538cbf234738f6a78676c078724aeba
gmaheswaranmca <gmaheswaranmca@gmail.com>
2024-07-20 17:52:05 +0530

    notes-app with menu active fixed - delete is coded

--- 20240720/year2-notes-app/app.py
+++ 20240720/year2-notes-app/app.py
@@ -9,12 +9,12 @@ def notes_list():
     notes = readAllNotes() #list of notes objects #
     for I in range(len(notes)):
         notes[I].sno = I + 1
-    return render_template('list.html',notes=notes)#
+    return render_template('list.html',notes=notes, menu='list')#
 @app.route("/create",methods=['GET','POST'])
 def notes_create(): ###all
     note = Note()
     if request.method == 'GET':
-        return render_template('create.html',note=note)
+        return render_template('create.html',note=note, menu='create')
     elif request.method == 'POST':
         note.title = request.form['title']
         note.notes = request.form['notes']
@@ -23,12 +23,12 @@ def notes_create(): ###all
 @app.route("/view/<id>")
 def notes_view(id,methods=['GET']):
     note = readNoteById(id)
-    return render_template('view.html', note=note)
+    return render_template('view.html', note=note, menu='view')
 @app.route("/edit/<id>",methods=['GET','POST'])
 def notes_edit(id):
     note = readNoteById(id)
     if request.method == 'GET':
-        return render_template('edit.html',note=note)
+        return render_template('edit.html',note=note, menu='edit')
     elif request.method == 'POST':
         note.title = request.form['title']
         note.notes = request.form['notes']
@@ -38,12 +38,19 @@ def notes_edit(id):
 @app.route("/",methods=['GET','POST'])
 def notes_index():
     if request.method == 'GET':
-        return render_template('index.html')
+        return render_template('index.html', menu='search')
     elif request.method == 'POST':
         title = request.form['title']
         notes_text = request.form['notes']
         notes = search(title,notes_text)
         for I in range(len(notes)):
             notes[I].sno = I + 1
-        return render_template('list.html',notes=notes)
+        return render_template('list.html',notes=notes, menu='list')
+
+@app.route("/delete",methods=['POST'])
+def notes_delete():
+    if request.method == 'POST':
+        id = request.form['id']
+        deleteNote(id)
+        return redirect('/list')
 
